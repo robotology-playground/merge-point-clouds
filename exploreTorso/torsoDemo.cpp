2bb9db5085441ee8036df8b7b4143eb0ab02e99c
Alessio Mauro <alessiomauro.franchi@polimi.it>
2014-07-29 12:33:20 +0200

    WIP

--- exploreTorso/torsoDemo.cpp
+++ exploreTorso/torsoDemo.cpp
@@ -9,15 +9,16 @@
 #include <yarp/dev/all.h>
 #include <yarp/math/Math.h>
 #include <iCub/ctrl/math.h>
+#include <yarp/os/RpcClient.h>
 
 
-#define LEFT_ARM_HOME_POS_X	-0.20
-#define LEFT_ARM_HOME_POS_Y	-0.35
-#define LEFT_ARM_HOME_POS_Z	0.12
+#define LEFT_ARM_HOME_POS_X	-0.25
+#define LEFT_ARM_HOME_POS_Y	-0.25
+#define LEFT_ARM_HOME_POS_Z	0.14
 
-#define RIGHT_ARM_HOME_POS_X	-0.20
-#define RIGHT_ARM_HOME_POS_Y	0.35
-#define RIGHT_ARM_HOME_POS_Z	0.12
+#define RIGHT_ARM_HOME_POS_X	-0.25
+#define RIGHT_ARM_HOME_POS_Y	0.25
+#define RIGHT_ARM_HOME_POS_Z	0.14
 
 #define MAX_ARM_TRAJ_TIME  0.8
 
@@ -29,13 +30,13 @@
 #define TORSO_ACCELERATION_PITCH	50.0
 #define TORSO_ACCELERATION_ROLL		50.0
 
-#define MAX_TORSO_VELOCITY 50.0
+#define MAX_TORSO_VELOCITY 40.0
 #define KP				15.0
-#define MAX_TORSO_TRAJ_TIME  3.0
+#define MAX_TORSO_TRAJ_TIME  4.0
 
-#define GAZE_HOME_POS_X		-0.4
+#define GAZE_HOME_POS_X		-0.5
 #define GAZE_HOME_POS_Y		0.0
-#define GAZE_HOME_POS_Z		0.0
+#define GAZE_HOME_POS_Z		0.1
 
 
 #define ACK                     VOCAB3('a','c','k')
@@ -70,7 +71,9 @@ using namespace iCub::ctrl;
 
 class TorsoModule:public RFModule
 {
-    RpcServer handlerPort; //a port to handle messages
+    RpcServer handlerPort;
+	RpcClient objectsPort;
+
     double period;
 	string moduleName;
 	string robotName;
@@ -105,6 +108,8 @@ class TorsoModule:public RFModule
 	double maxTorsoTrajTime;
 	double maxArmTrajTime;
 
+	bool running;
+
 	public:
 
     double getPeriod()
@@ -121,9 +126,15 @@ class TorsoModule:public RFModule
 	{
 		Matrix R(3,3);
 		R(0,0)= -1.0; R(0,1)= 0.0; R(0,2)= 0.0;
-		R(1,0)= 0.0; R(1,1)= 0.0; R(1,2)=-1.0; 
-		R(2,0)=-0.0; R(2,1)= -1.0; R(2,2)= 0.0;
-		leftArmHomeOrientation = rightArmHomeOrientation = dcm2axis(R);
+		R(1,0)= 0.0; R(1,1)= sin(CTRL_DEG2RAD*30); R(1,2)=-cos(CTRL_DEG2RAD*30); 
+		R(2,0)=-0.0; R(2,1)= -cos(CTRL_DEG2RAD*30); R(2,2)= -sin(CTRL_DEG2RAD*30);
+		rightArmHomeOrientation = dcm2axis(R);
+
+		R(0,0)= -1.0; R(0,1)= 0.0; R(0,2)= 0.0;
+		R(1,0)= 0.0; R(1,1)= -sin(CTRL_DEG2RAD*30); R(1,2)=-cos(CTRL_DEG2RAD*30); 
+		R(2,0)=-0.0; R(2,1)= -cos(CTRL_DEG2RAD*30); R(2,2)= sin(CTRL_DEG2RAD*30);
+		leftArmHomeOrientation = dcm2axis(R);
+
 		return true;
 	}
 
@@ -168,7 +179,8 @@ class TorsoModule:public RFModule
 		while (norm(torsoVelocityCommand)>0.1){
 			if(Time::now()-time>maxTorsoTrajTime){
 				cout<<"Max time reached."<<endl;
-				break;
+				itorsoVelocity->stop();
+				return true;
 			}
 			if (norm(torsoVelocityCommand)>maxTorsoVelocity)
 				torsoVelocityCommand = maxTorsoVelocity/norm(torsoVelocityCommand)*torsoVelocityCommand;
@@ -176,16 +188,15 @@ class TorsoModule:public RFModule
 			itorsoVelocity->velocityMove(torsoVelocityCommand.data());
 			
 			if (!iTorsoEncoder->getEncoders(torsoActualJoints.data())){
-				cout<<"Error in reading encoders."<<endl;				
+				cout<<"Error in reading encoders."<<endl;		
+				itorsoVelocity->stop();
 				return false;
 			}
 			
 			torsoVelocityCommand = kp * (target-torsoActualJoints);
-			//cout<<"Actual: " << torsoActualJoints[0] << " - " << torsoActualJoints[1] << " - "<< torsoActualJoints[2]<<endl;
-			Time::delay(0.02);
+			Time::delay(0.05);
 
 		}
-		cout<<"Stopping velocity."<<endl;
 		itorsoVelocity->stop();
 		return true;
 	}
@@ -225,7 +236,6 @@ class TorsoModule:public RFModule
 							return true;
 						}
 			else{
-				
 				icartLeft->goToPoseSync(leftArmHomePosition,leftArmHomeOrientation);
 				icartRight->goToPoseSync(rightArmHomePosition,rightArmHomeOrientation);
 				igaze->lookAtFixationPoint(gazeHomePosition);
@@ -237,7 +247,41 @@ class TorsoModule:public RFModule
 				return true;
 			}
 		case LOOK_AT:
-			if(command.size()==4){
+			if(command.size()==2){
+				Bottle bAsk,bReply, bGet;
+				bAsk.addVocab(Vocab::encode("ask"));
+				Bottle &bTempAsk=bAsk.addList().addList();
+				bTempAsk.addString("name");
+				bTempAsk.addString("==");
+				bTempAsk.addString(command.get(1).asString());
+				cout<<"Sending request"<<endl;
+				objectsPort.write(bAsk,bReply);
+				
+				if(bReply.size()==0 || bReply.get(0).asVocab()!=Vocab::encode("ack") || bReply.get(1).asList()->check("id")==false || 
+					bReply.get(1).asList()->find("id").asList()->size()==0){
+						reply.addString("Oject not found in db.");
+						return true;
+				}
+
+
+				bGet.addVocab(Vocab::encode("get"));
+				Bottle &bTempGet=bGet.addList().addList();
+				bTempGet.addString("id");
+				bTempGet.addInt(bReply.get(1).asList()->find("id").asList()->get(0).asInt());
+				objectsPort.write(bGet,bReply);
+
+				if(bReply.size()==0 || bReply.get(0).asVocab()!=Vocab::encode("ack")){
+						reply.addString("Error in ");
+						return true;
+				}
+
+
+
+				reply.addString("Ok, request done.");
+				return true;
+			}
+
+			else if(command.size()==4){
 				Vector gazePosition(3);
 				gazePosition[0] = command.get(1).asDouble();
 				gazePosition[1] = command.get(2).asDouble();
@@ -248,7 +292,7 @@ class TorsoModule:public RFModule
 				return true;
 			}
 			else{
-				reply.addString("Wrong number of coordinates.");
+				reply.addString("Wrong number of parameters for lookAt.");
 				return true;
 			}
 		case TRACK:
@@ -331,21 +375,28 @@ class TorsoModule:public RFModule
 			return true;
 			
 		case RUN: 
+			running = true;
 			reply.addString("Ready for exploration. next or stop?");
 			return true;
 		case NEXT:
-			exploreTorso(waypoints.getRow(index));
-			index++;
-			if (index > 4){
-				reply.addString("Waypoint reached. End of waypoints.");
-				index = 0;
+			if(running){
+				exploreTorso(waypoints.getRow(index));
+				index++;
+				if (index > 4){
+					running = false;
+					index = 0;
+					reply.addString("Waypoint reached. End of waypoints.");
+					}
+				else{
+					reply.addString("Waypoint reached. next or stop?");
+				}
 			}
-			else{
-				reply.addString("Waypoint reached. next or stop?");
-			}
+			else
+				reply.addString("Not running.");
 			return true;
 		case STOP:
 			index = 0;
+			running = false;
 			reply.addString("Run stopped. Index reset.");
 			return true;
 			
@@ -374,6 +425,12 @@ class TorsoModule:public RFModule
 
 		handlerPort.open(("/"+moduleName+"/rpc:i").c_str());
         attach(handlerPort);
+
+		objectsPort.open(("/"+moduleName+"/OPC:io").c_str());
+		if(!objectsPort.addOutput("/objectsPropertiesCollector/rpc")){
+			cout<<"Error connecting to OPC client!"<<endl;
+			return false;
+		}
 
 
 		Property optionGaze;
@@ -411,10 +468,10 @@ class TorsoModule:public RFModule
 		cout<<"MAX ARM TRAJ TIME: "<< maxArmTrajTime<<endl;
 		icartLeft->setTrajTime(maxArmTrajTime);
 
-		icartLeft->getDOF(curDof);
+		/*icartLeft->getDOF(curDof);
         newDof=curDof;
 		newDof[3]=0;
-		icartLeft->setDOF(newDof,curDof);
+		icartLeft->setDOF(newDof,curDof);*/
 		
 
 		Property rightArmOption;
@@ -468,25 +525,31 @@ class TorsoModule:public RFModule
 
 		waypoints.resize(5,3);
 		waypoints(0,0) = 10.0; waypoints(0,1) = 10.0; waypoints(0,2) = 20.0; 
-		waypoints(1,0) = 30.0; waypoints(1,1) = 20.0; waypoints(1,2) = 20.0; 
+		waypoints(1,0) = 30.0; waypoints(1,1) = 20.0; waypoints(1,2) = 25.0; 
 		waypoints(2,0) = 0.0; waypoints(2,1) = 0.0; waypoints(2,2) = 0.0; 
 		waypoints(3,0) = -10.0; waypoints(3,1) = -10.0; waypoints(3,2) = 20.0; 
-		waypoints(4,0) = -30.0; waypoints(4,1) = -20.0; waypoints(4,2) = 20.0; 
+		waypoints(4,0) = -30.0; waypoints(4,1) = -20.0; waypoints(4,2) = 25.0; 
+		
 		index = 0;
+		running = false;
 		cout<<endl;
         return true;
     }
 
     bool interruptModule()
     {
+
         cout<<"Interrupt caught!"<<endl;
 		cout<<endl;
+		handlerPort.interrupt();
+		objectsPort.interrupt();
         return true;
     }
 
     bool close()
     {
         handlerPort.close();
+		objectsPort.close();
 
 		igaze->stopControl();
 		igaze->restoreContext(startupGazeContextID);
